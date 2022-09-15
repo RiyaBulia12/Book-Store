188a72b02356076ecbbfa812d2f978683d42aad0
Apple <riyabulia12@gmail.com>
2022-09-16 01:17:36 +0530

    Add navbar, routing and css

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { Link, Route, Routes } from 'react-router-dom';
+import Categories from './components/Categories';
+import Books from './components/Books';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="panel-bg flex center">
+        <h1 className="logo m-auto">
+          Bookstore CMS
+        </h1>
+        <div className="flex-1">
+          <ul className="flex uppercase">
+            <li>
+              <Link to="/books">Books</Link>
+            </li>
+            <li>
+              <Link to="/categories">Categories</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="p-1 w-10 h-10 rounded-full ring-1 ring-gray-300 profile" />
       </header>
+
+      <hr className="h-px bg-gray-200 border-0" />
+      <main>
+        <Routes>
+          <Route path="/books" element={<Books />} />
+          <Route path="/categories" element={<Categories />} />
+        </Routes>
+      </main>
     </div>
   );
 }
